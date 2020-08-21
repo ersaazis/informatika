85e48feea91f1368025a48eb22e6a045c91c0b33
ersaazis <eam24maret@gmail.com>
2020-08-21 16:16:46 +0700

    update bug

--- rebuild.sh
+++ rebuild.sh
@@ -1,15 +1,18 @@
-# REQ
-# sudo apt update
-# sudo apt install -y php-curl php-imagick php-mbstring php-gd poppler-utils
-# sudo sed -i 's/<policy domain="coder" rights="none" pattern="PDF" \/>/<policy domain="coder" rights="read|write" pattern="PDF" \/>/g' /etc/ImageMagick-6/policy.xml
-# php artisan queue:work database --queue=dataDosen,dataDokumen
 #!/bin/bash
+# REQ
+sudo apt update
+sudo apt install -y git zip ocrmypdf php php-mysql php-curl php-imagick php-mbstring php-gd php-xml poppler-utils
+sudo sed -i 's/<policy domain="coder" rights="none" pattern="PDF" \/>/<policy domain="coder" rights="read|write" pattern="PDF" \/>/g' /etc/ImageMagick-6/policy.xml
 curl https://getcomposer.org/installer > composer-setup.php
-#export HOME=/home/username && ea-php72 -c php.ini composer-setup.php
-ea-php72 -c php.ini composer-setup.php
-ea-php72 -r "unlink('composer-setup.php');"
-ea-php72 -c php.ini composer.phar install
-ea-php72 -r "unlink('composer.phar');"
+#export HOME=/home/username && php -c php.ini composer-setup.php
 mv app/Http/Controllers app/Http/Controllersx
-ea-php72 artisan crud:rebuild
+php -c php.ini composer-setup.php
+php -r "unlink('composer-setup.php');"
+php -c php.ini composer.phar install
+php artisan migrate
+php -c php.ini composer.phar update
+php artisan crud:seed
 mv app/Http/Controllersx app/Http/Controllers
+php -r "unlink('composer.phar');"
+php artisan package:discover --ansi
+php artisan queue:work database --queue=dataDosen,dataDokumen
